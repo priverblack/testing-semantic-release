28d51a6941379a1000acc6a8667ea998dfe65ba2
Paulo Preto <paulopreto.dev@gmail.com>
2025-07-15 22:37:12 +0100

    chore: add docs to subtract()

--- src/utils.ts
+++ src/utils.ts
@@ -6,4 +6,10 @@
  */
 export const add = (a: number, b: number) => a + b;
 
+/**
+ * Function to subtract two numbers
+ * @param a - the first number to subtract
+ * @param b - the second number to subtract
+ * @returns
+ */
 export const subtract = (a: number, b: number) => a - b;
