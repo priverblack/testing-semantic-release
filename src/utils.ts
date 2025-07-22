a882bd7a61607691791109c46b23c89574c30444
Paulo Preto <paulopreto.dev@gmail.com>
2025-07-22 18:29:30 +0100

    docs: add JSDoc comments to describe times function

--- src/utils.ts
+++ src/utils.ts
@@ -14,4 +14,10 @@ export const add = (a: number, b: number) => a + b;
  */
 export const subtract = (a: number, b: number) => a - b;
 
+/**
+ * Function to multiply two numbers
+ * @param a - the first number to multiply
+ * @param b - the second number to multiply
+ * @returns
+ */
 export const times = (a: number, b: number) => a * b;
