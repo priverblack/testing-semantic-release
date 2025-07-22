d212df7bae2399f21c36c10e98d3f5cb4d85d21f
Paulo Preto <paulopreto.dev@gmail.com>
2025-07-22 18:48:45 +0100

    test: add tests for the utility functions

---
+++ src/tests/add.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest';
+import { add } from '../add.js';
+
+describe("add", () => {
+  test("basic test", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+});
